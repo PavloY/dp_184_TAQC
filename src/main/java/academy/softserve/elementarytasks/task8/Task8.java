491fc7f58d875757f9c11e41136c9892d35c8011
Pavlo <pavlo.yakovlev@gmail.com>
2020-02-10 15:13:47 +0200

    Update Task8.java

--- src/main/java/academy/softserve/elementarytasks/task8/Task8.java
+++ src/main/java/academy/softserve/elementarytasks/task8/Task8.java
@@ -7,37 +7,37 @@ import java.util.Scanner;
 public class Task8 {
 
   public static void main(String[] args) {
-
-    setType();
-    setLength();
-    setRange();
-
     int[] arr = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765,
             10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309,
             3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141,
             267914296, 433494437, 701408733};
 
-    int lower=140;
-    int upper=620;
-    range(arr, lower, upper);
+
+    setType();
+    setLength();
+    range(arr, setRange(), setRangeUpper());
 
 
   }
 
 
-  private static void setRange() {
-    int lower;
-    int upper;
+  private static int setRange() {
+    int lower = 0;
+
     Scanner sc = new Scanner(System.in);
 
-      System.out.println("set lower range:");
-      String lowerStr = sc.nextLine();
-      try {
-        lower = Integer.parseInt(lowerStr);
-      } catch (NumberFormatException ex) {
-        System.out.println("Incorrect range!");
-      }
-
+    System.out.println("set lower range:");
+    String lowerStr = sc.nextLine();
+    try {
+      lower = Integer.parseInt(lowerStr);
+    } catch (NumberFormatException ex) {
+      System.out.println("Incorrect range!");
+    }
+    return lower;
+  }
+    private static int setRangeUpper() {
+    int upper = 0;
+    Scanner sc = new Scanner(System.in);
       System.out.println("set upper range:");
       String upperStr = sc.nextLine();
       try {
@@ -47,7 +47,7 @@ public class Task8 {
       }
 
     System.out.println("have a range");
-
+      return upper;
   }
 
   private static void setLength() {
