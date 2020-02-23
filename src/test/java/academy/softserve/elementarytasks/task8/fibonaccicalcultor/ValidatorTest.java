31acccb73784fef0fd5e99dda3f69ecc70fcbae2
Pavlo <pavlo.yakovlev@gmail.com>
2020-02-23 23:19:22 +0200

    Update ValidatorTest.java

--- src/test/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/ValidatorTest.java
+++ src/test/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/ValidatorTest.java
@@ -1,8 +1,32 @@
 package academy.softserve.elementarytasks.task8.fibonaccicalcultor;
 
-import java.math.BigDecimal;
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.math.BigInteger;
 
 public class ValidatorTest {
+
+  Validator validator = new Validator();
+
+  @Test
+  public void testValidatorInt() {
+    Assert.assertEquals(validator.validateInput("12321"), "12321");
+  }
+
+  @Test
+  public void testValidatorMinus() {
+    Assert.assertEquals(validator.validateInput("-12321"), "12321");
+  }
+
+  @Test
+  public void testConvertStrToBigInt() {
+    Assert.assertEquals(validator.convertStrToBigInt("12321"), new BigInteger("12321"));
+  }
+
+  @Test
+  public void testConvertStrToInt() {
+    Assert.assertEquals(validator.convertStrToInt("12321"), 12321);
+  }
 
 }
