d5b6f7a75cf8340a3d5d5ebc0488f138844cc910
Pavlo <pavlo.yakovlev@gmail.com>
2020-02-24 13:42:19 +0200

    Update FibonacciSupplier.java

--- src/main/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/FibonacciSupplier.java
+++ src/main/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/FibonacciSupplier.java
@@ -1,26 +1,23 @@
 package academy.softserve.elementarytasks.task8.fibonaccicalcultor;
 
 import java.math.BigInteger;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
 
 
 public class FibonacciSupplier {
 
-  private Set<String> setFibonacci;
   private boolean isLength = false;
   private boolean isRange = false;
   private BigInteger previousNumbers = new BigInteger("0");
   private BigInteger nextNumbers = new BigInteger("1");
+  ArrayList<String> setFibonacci = new ArrayList <String> ();
 
 
-
-  public Set<String> creatorFibonacci(BigInteger lower, BigInteger upper) {
-    setFibonacci = new HashSet<>();
+  public ArrayList<String> creatorFibonacci(BigInteger lower, BigInteger upper) {
     do {
       if ((previousNumbers.compareTo(lower) == 1 || previousNumbers.compareTo(lower) == 0)
               && (previousNumbers.compareTo(upper) == -1 || previousNumbers.compareTo(upper) == 0)) {
-        setFibonacci.add((previousNumbers).toString());
+          setFibonacci.add((previousNumbers).toString());
       }
       if (previousNumbers.compareTo(upper) == 1) {
         isRange = true;
@@ -30,8 +27,7 @@ public class FibonacciSupplier {
     return setFibonacci;
   }
 
-  public Set<String> creatorFibonacci(BigInteger length) {
-    setFibonacci = new HashSet<>();
+  public ArrayList<String> creatorFibonacci(BigInteger length) {
     do {
       if (previousNumbers.toString().length() == Integer.parseInt(String.valueOf(length))) {
         setFibonacci.add((previousNumbers).toString());
