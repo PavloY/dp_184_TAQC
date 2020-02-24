4b1e072c9f92d0879f9b7e1de63fbf4e9f0da5f4
Pavlo <pavlo.yakovlev@gmail.com>
2020-02-24 22:51:04 +0200

    Update FibonacciSupplier.java

--- src/main/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/FibonacciSupplier.java
+++ src/main/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/FibonacciSupplier.java
@@ -4,35 +4,37 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class FibonacciSupplier {
-
+    private boolean isRangeExist = false;
+    private boolean isLengthExist;
     private BigInteger previousNumbers = new BigInteger("0");
     private BigInteger nextNumbers = new BigInteger("1");
     ArrayList<String> setFibonacci = new ArrayList<String>();
 
+
     public ArrayList<String> creatorFibonacci(BigInteger lower, BigInteger upper) {
-        boolean isRange = false;
+        boolean isRange;
         do {
-            if ((previousNumbers.compareTo(lower) == 1 || previousNumbers.compareTo(lower) == 0)
-                    && (previousNumbers.compareTo(upper) == -1 || previousNumbers.compareTo(upper) == 0)) {
+            if (isRangeExist(lower, upper)) {
                 setFibonacci.add((previousNumbers).toString());
             }
-            isRange = (previousNumbers.compareTo(upper) == 1);
+            isRange = (previousNumbers.compareTo(upper) > 0);
             countSequenceFibonacci();
         } while (!isRange);
         return setFibonacci;
     }
 
     public ArrayList<String> creatorFibonacci(BigInteger length) {
-        boolean isLength = false;
+        boolean isLength;
         do {
-            if (previousNumbers.toString().length() == Integer.parseInt(String.valueOf(length))) {
+            if (isLengthExist(length)){
                 setFibonacci.add((previousNumbers).toString());
             }
-            isLength = (previousNumbers.toString().length() > Integer.parseInt(String.valueOf(length)));
+            isLength = (previousNumbers.toString().length() > length.toString().length());
             countSequenceFibonacci();
         } while (!isLength);
         return setFibonacci;
     }
+
 
     private void countSequenceFibonacci() {
         previousNumbers = previousNumbers.add(nextNumbers);
@@ -40,4 +42,20 @@ public class FibonacciSupplier {
         previousNumbers = nextNumbers;
         nextNumbers = sums;
     }
+
+    private boolean isRangeExist(BigInteger lower, BigInteger upper) {
+        if (((previousNumbers.compareTo(lower) > 0) || (previousNumbers.compareTo(lower) == 0))
+                && ((previousNumbers.compareTo(upper) < 0) || (previousNumbers.compareTo(upper) == 0))) {
+            isRangeExist = true;
+        }
+        return isRangeExist;
+    }
+
+    private boolean isLengthExist(BigInteger length) {
+        if (previousNumbers.toString().length() == Integer.parseInt(String.valueOf(length))) {
+            isLengthExist = true;
+        }
+        return isLengthExist;
+    }
 }
+
