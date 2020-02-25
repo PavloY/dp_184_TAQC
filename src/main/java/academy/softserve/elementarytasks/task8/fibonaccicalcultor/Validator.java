30faefd28b367fb6023ba096e62dabc9833246b0
Pavlo <pavlo.yakovlev@gmail.com>
2020-02-25 17:45:57 +0200

    Update Validator.java

--- src/main/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/Validator.java
+++ src/main/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/Validator.java
@@ -4,36 +4,36 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class Validator {
-  private BigInteger inputValid;
-  private BigInteger inputValidInt;
+    private BigInteger inputValid;
+    private BigInteger inputValidInt;
 
-  public int convertStrToInt(String text) {
-    try {
-      Integer.parseInt(text);
-    }catch (NumberFormatException ex) {
-      ConsoleOutPut print = new ConsoleOutPut();
-      print.showResult(Parametr.SHOW_ERROR);
+    public int convertStrToInt(String text) {
+        try {
+            Integer.parseInt(text);
+        } catch (NumberFormatException ex) {
+            ConsoleOutPut print = new ConsoleOutPut();
+            print.showResult(Parametr.SHOW_ERROR);
+        }
+        return Integer.parseInt(text);
     }
-    return Integer.parseInt(text);
-  }
 
-  public BigInteger convertStrToBigInt(String string){
-    try {
-      inputValid = new BigDecimal(string).toBigInteger();
-    }catch (NumberFormatException ex) {
-      ConsoleOutPut print = new ConsoleOutPut();
-      print.showResult(Parametr.SHOW_ERROR);
+    public BigInteger convertStrToBigInt(String string) {
+        try {
+            inputValid = new BigDecimal(string).toBigInteger();
+        } catch (NumberFormatException ex) {
+            ConsoleOutPut print = new ConsoleOutPut();
+            print.showResult(Parametr.SHOW_ERROR);
+        }
+        return inputValid;
     }
-    return inputValid;
-  }
 
-  public String validateInput(String inputValidate ) {
-    try {
-      inputValidInt = new BigDecimal(inputValidate).toBigInteger().abs();
-    } catch (NumberFormatException ex) {
-      ConsoleOutPut print = new ConsoleOutPut();
-      print.showResult(Parametr.SHOW_ERROR);
+    public String validateInput(String inputValidate) {
+        try {
+            inputValidInt = new BigDecimal(inputValidate).toBigInteger().abs();
+        } catch (NumberFormatException ex) {
+            ConsoleOutPut print = new ConsoleOutPut();
+            print.showResult(Parametr.SHOW_ERROR);
+        }
+        return inputValidInt.toString();
     }
-    return inputValidInt.toString();
-  }
 }
