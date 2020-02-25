a7e306d1881c56d1fda579f58f4132fd630c8622
Pavlo <pavlo.yakovlev@gmail.com>
2020-02-25 17:45:40 +0200

    Update ConsoleOutPut.java

--- src/main/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/ConsoleOutPut.java
+++ src/main/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/ConsoleOutPut.java
@@ -6,15 +6,13 @@ import java.util.ArrayList;
 
 public class ConsoleOutPut {
 
+    public void showResult(String text) {
+        System.out.println(text);
+    }
 
-  public void showResult(String text) {
-    System.out.println(text);
-  }
-
-
-  public void showFibonacci(ArrayList<String> printSet) {
-    System.out.print(printSet);
-  }
+    public void showFibonacci(ArrayList<String> printSet) {
+        System.out.print(printSet);
+    }
 
 
 }
