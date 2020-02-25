b90e6b964ec2b8ed55660772d9f786f3fc5242bf
Pavlo <pavlo.yakovlev@gmail.com>
2020-02-25 15:38:59 +0200

    Update ValidatorTest.java

--- src/test/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/ValidatorTest.java
+++ src/test/java/academy/softserve/elementarytasks/task8/fibonaccicalcultor/ValidatorTest.java
@@ -1,16 +1,44 @@
 package academy.softserve.elementarytasks.task8.fibonaccicalcultor;
 
+import academy.softserve.elementarytasks.task9.palindrome.Validator;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(DataProviderRunner.class)
 public class ValidatorTest {
 
-  Validator validator = new Validator();
+  Validator validator;
+
+  @DataProvider
+  public static Object[][] fibonacci(){
+    return new Object[][] {
+            {"12321","12321"},
+            {"12321","12321"},
+    };
+  }
 
   @Test
+  @UseDataProvider("fibonacci")
+  public void testValidator(String expected, String input) {
+
+    validator = new Validator(input);
+    String actual = validator.validateInput(null);
+
+    Assert.assertEquals(expected, actual);
+  }
+/*
+  @Test
   public void testValidatorInt() {
+
     Assert.assertEquals(validator.validateInput("12321"), "12321");
   }
 
@@ -28,5 +56,7 @@ public class ValidatorTest {
   public void testConvertStrToInt() {
     Assert.assertEquals(validator.convertStrToInt("12321"), 12321);
   }
+
+ */
 
 }
